732ca20969ccee0fda01832dd8eb320d8c201506
Djalma Araujo <djalma.araujo@gmail.com>
2020-01-28 16:10:03 -0300

    use char code at for generic usage

--- extension.js
+++ extension.js
@@ -1,39 +1,15 @@
 const vscode = require('vscode');
+
 const editor = vscode.window.activeTextEditor;
-
-const chars = [
-	{
-		char: `'`,
-		value: '&apos;',
-	},
-	{
-		char: `>`,
-		value: '&gt;',
-	},
-	{
-		char: `"`,
-		value: '&quot;',
-	},
-	{
-		char: `{`,
-		value: '&#123;',
-	},
-	{
-		char: `}`,
-		value: '&#125;',
-	}
-]
-
-const findChar = (char) => chars.find((i) => char.toString() === i.char)
 
 async function escapeEntities () {
 	const selection = editor.selection;
 	const text = editor.document.getText(selection);
-	const char = findChar(text);
+	const char = `&#${text.charCodeAt()};`;
 
 	if (char) {
 		editor.edit(builder => {
-			builder.replace(selection, char.value)
+			builder.replace(selection, char)
 		})
 	}
 }
